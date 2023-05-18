60a268e99cb4b72d9f0898c7d60d19a75e7ec7fc
Akash Patel <pppatelakash@gmail.com>
2023-05-18 18:24:32 +0530

    Reference GitHub Actions secret directly

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
+// Server-side code
 const express = require('express');
+const fetch = require('node-fetch');
+
 const app = express();
 const port = 3000;
 
-// Serve static files
 app.use(express.static(__dirname));
 
-// Define API endpoint
 app.get('/weather', (req, res) => {
-  // Get latitude and longitude from the request query parameters
-  const latitude = req.query.lat;
-  const longitude = req.query.lon;
+  const { lat, lon } = req.query;
+  const apiKey = process.env.OPENWEATHERMAP_API_KEY; // Reference GitHub Actions secret directly
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
-
-
-   // Make API request to fetch weather data
-   const apiKey = process.env.API_KEY;
-   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  // // Make API request to fetch weather data
-  // const apiKey = "15f3924c615083852ae4bd2e08e025ec";
-  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-  // Dynamic import of node-fetch
-  require('node-fetch').default(url)
-    .then(response => response.json())
-    .then(data => {
-      // Process the weather data and send the response
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
       res.json(data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
       res.status(500).json({ error: 'An error occurred' });
     });
@@ -37,7 +27,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
